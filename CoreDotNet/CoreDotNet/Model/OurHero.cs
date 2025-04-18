50507c7e5f59402de3329e0ff4d319fe3db4c725
Sreekanth430-ui <59555715+Sreekanth430-ui@users.noreply.github.com>
2025-04-18 11:47:45 +0530

    Add files via upload

---
+++ CoreDotNet/CoreDotNet/Model/OurHero.cs
@@ -0,0 +1,17 @@
+﻿namespace CoreDotNet.Model
+{
+    public class OurHero
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public string LastName { get; set; } = string.Empty;
+        public bool isActive { get; set; } = true;
+    }
+    //public class OurHero
+    //{
+    //    public required string FirstName { get; set; }
+    //    public string LastName { get; set; } = string.Empty;
+    //    public bool isActive { get; set; } = true;
+    //}
+
+}
